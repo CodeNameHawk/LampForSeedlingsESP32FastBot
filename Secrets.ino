cb6694c29466c43cbe4272976979d7028b976d1d
Marian <kumahe@gmail.com>
2025-03-13 20:44:16 +0200

    v.1 2025.03.13

---
+++ Secrets.ino
@@ -0,0 +1,6 @@
+const char *ssid = "SSID";                      // SSID WiFi network
+const char *pass = "PASSWORD";                  // Password  WiFi network
+const char *token = "xxxxxx:xxxxxxxxxxxxxxxxx"; // @LampForSeedlingsESP32FastBot
+
+// https://t.me/JsonDumpBot
+const char *userid = "xxxxxxxxx"; // User ID
